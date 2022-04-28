3099578ae6b82b159eaf1e53cc93395c43b1df9d
dewslyse <dewslyse@gmail.com>
2022-04-28 20:23:57 +0000

    Update Categories component

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 const Categories = () => (
-  <div>
-    <h1>Categories</h1>
-    <button type="button">Check status</button>
-  </div>
+  <button type="button">Check status</button>
 );
 
 export default Categories;
