dcf1e7455282445ce393db56fce744152c649291
dewslyse <dewslyse@gmail.com>
2022-05-02 23:59:11 +0000

    Configure redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore } from 'redux';
+import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
+
+const rootReducer = combineReducers({
+  booksReducer,
+  categoriesReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
