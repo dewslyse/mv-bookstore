ec9b24dd818c30517d233f16713808fc2533b0e3
dewslyse <dewslyse@gmail.com>
2022-05-02 23:58:31 +0000

    Create book actions and reducer

---
+++ src/redux/books/books.js
@@ -0,0 +1,34 @@
+const ADDED_BOOK = 'mv-bookstore/books/ADDED_BOOK';
+const REMOVED_BOOK = 'mv-bookstore/books/REMOVED_BOOK';
+
+const initialState = [];
+
+// Action Creators
+export const addBook = (title, author) => ({
+  type: ADDED_BOOK,
+  payload: { title, author },
+});
+
+export const removeBook = (id) => ({
+  type: REMOVED_BOOK,
+  payload: id,
+});
+
+// Reducer
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDED_BOOK:
+      return [
+        ...state,
+        action.payload,
+      ];
+    case REMOVED_BOOK:
+      return [
+        ...state.filter((book) => (book.id !== action.id)),
+      ];
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
