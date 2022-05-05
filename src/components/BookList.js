1fb48cb9c76e4de9ad6de82c6298d2034d539515
dewslyse <dewslyse@gmail.com>
2022-05-05 13:08:48 +0000

    Retrieve books from API

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBooks } from '../redux/books/books';
 import Book from './Book';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  console.log(books);
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
 
   return (
     <ul>
       {books.map((book) => (
-        <Book key={book.id} book={book} />
+        <Book key={book.item_id} book={book} />
       ))}
     </ul>
   );
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,27 +1,47 @@
 import { v4 as uuidv4 } from 'uuid';
+import { fetchBooksFromAPI } from '../../api/api';
 
 const ADDED_BOOK = 'mv-bookstore/books/ADDED_BOOK';
 const REMOVED_BOOK = 'mv-bookstore/books/REMOVED_BOOK';
+const FETCHED_BOOKS = 'mv-bookstore/books/FETCHED_BOOKS';
 
 const initialState = [
-  {
-    id: uuidv4(),
-    title: 'The power of habit',
-    author: 'Charles Duhigg',
-  },
-  {
-    id: uuidv4(),
-    title: 'Atomic Habits',
-    author: 'James Clear',
-  },
-  {
-    id: uuidv4(),
-    title: 'Thinking for a change',
-    author: 'John C. Maxwell',
-  },
+  // {
+  //   id: uuidv4(),
+  //   title: 'The power of habit',
+  //   author: 'Charles Duhigg',
+  // },
+  // {
+  //   id: uuidv4(),
+  //   title: 'Atomic Habits',
+  //   author: 'James Clear',
+  // },
+  // {
+  //   id: uuidv4(),
+  //   title: 'Thinking for a change',
+  //   author: 'John C. Maxwell',
+  // },
 ];
 
 // Action Creators
+export const getBooks = () => async (dispatch) => {
+  const allBooks = await fetchBooksFromAPI();
+
+  const payload = Object.keys(allBooks).map((key) => {
+    const { title, author, category } = allBooks[key][0];
+    return {
+      item_id: key,
+      title,
+      author,
+      category,
+    };
+  });
+  dispatch({
+    type: FETCHED_BOOKS,
+    payload,
+  });
+};
+
 export const addBook = (title, author) => ({
   type: ADDED_BOOK,
   payload: { title, author, id: uuidv4() },
@@ -35,6 +55,8 @@ export const removeBook = (id) => ({
 // Reducer
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHED_BOOKS:
+      return action.payload;
     case ADDED_BOOK:
       return [
         ...state,
@@ -42,7 +64,7 @@ const booksReducer = (state = initialState, action) => {
       ];
     case REMOVED_BOOK:
       return [
-        ...state.filter((book) => (book.id !== action.payload)),
+        ...state.filter((book) => (book.item_id !== action.payload)),
       ];
     default:
       return state;
