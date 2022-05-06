a0357c3b6d2bd3633ba94259c7339254c1e150ff
dewslyse <dewslyse@gmail.com>
2022-05-06 19:58:03 +0000

    Style header section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { IoIosPerson } from 'react-icons/io';
 
 const Nav = () => (
-  <header>
-    <div>
-      <div>Bookstore CMS</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Books</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-        </ul>
-      </nav>
+  <header className="header">
+    <div className="navbar container">
+      <div className="logo-nav">
+        <div className="logo">Bookstore CMS</div>
+        <nav>
+          <ul className="nav">
+            <li>
+              <NavLink to="/" className="nav-link">
+                Books
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/categories" className="nav-link">
+                Categories
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="user-box">
+        <IoIosPerson className="user" />
+      </div>
     </div>
   </header>
 );
